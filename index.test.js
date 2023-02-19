3f4dfafcfe085f57560612fe6e40086fd2b534e3
Muhammad <muhammadalnabhani@gmail.com>
2023-02-19 21:58:52 +0000

    Eager loading test completed

--- index.test.js
+++ index.test.js
@@ -34,69 +34,144 @@ describe("model creation tests", () => {
     expect(cheese1.title).toBe("Cheddar");
     expect(cheese1.description).toBe("scrumptious");
   });
+});
 
-  describe("Association test", () => {
-    test("User can have many Boards", async () => {
-      await sequelize.sync({ force: true });
+describe("Association test", () => {
+  test("User can have many Boards", async () => {
+    await sequelize.sync({ force: true });
 
-      let user1 = await User.create({
-        name: "Jonny",
-        email: "jonny@gmail.com",
-      });
-
-      let board1 = await Board.create({
-        type: "checkered",
-        description: "Black & White",
-        rating: 9,
-      });
-
-      let board2 = await Board.create({
-        type: "Soft-cheese",
-        description: "green",
-        rating: 8,
-      });
-
-      await user1.addBoard(board1);
-      await user1.addBoard(board2);
-
-      let boards = await user1.getBoards();
-
-      expect(boards.length).toBe(2);
-      expect(boards[0] instanceof Board).toBeTruthy;
+    let user1 = await User.create({
+      name: "Jonny",
+      email: "jonny@gmail.com",
     });
+
+    let board1 = await Board.create({
+      type: "checkered",
+      description: "Black & White",
+      rating: 9,
+    });
+
+    let board2 = await Board.create({
+      type: "Soft-cheese",
+      description: "green",
+      rating: 8,
+    });
+
+    await user1.addBoard(board1);
+    await user1.addBoard(board2);
+
+    let boards = await user1.getBoards();
+
+    expect(boards.length).toBe(2);
+    expect(boards[0] instanceof Board).toBeTruthy;
+  });
+});
+
+describe("Association test2", () => {
+  test("Board can have many Cheeses", async () => {
+    await sequelize.sync({ force: true });
+
+    let board1 = await Board.create({
+      type: "checkered",
+      description: "Black & White",
+      rating: 9,
+    });
+
+    let cheese1 = await Cheese.create({
+      title: "Brie",
+      description: "Soft & delicate",
+    });
+
+    let cheese2 = await Cheese.create({
+      title: "Quark",
+      description: "Squidgy and slimy",
+    });
+
+    await board1.addCheese(cheese1);
+    await board1.addCheese(cheese2);
+
+    let cheeses = await board1.getCheeses();
+
+    expect(cheeses.length).toBe(2);
+    expect(cheeses[0] instanceof Cheese).toBe(true);
+  });
+});
+
+test("A Cheese can be on many Boards", async () => {
+  await sequelize.sync({ force: true });
+  let cheese1 = await Cheese.create({
+    title: "Roquefort",
+    description: "Yellow & green",
   });
 
-  describe("Association test2", () => {
-    test("Board can have many Cheeses", async () => {
-      await sequelize.sync({ force: true });
-
-      let board1 = await Board.create({
-        type: "checkered",
-        description: "Black & White",
-        rating: 9,
-      });
-
-      let cheese1 = await Cheese.create({
-        title: "Brie",
-        description: "Soft & delicate",
-      });
-
-      let cheese2 = await Cheese.create({
-        title: "Quark",
-        description: "Squidgy and slimy",
-      });
-
-      await board1.addCheese(cheese1);
-      await board1.addCheese(cheese2);
-
-      let cheeses = await board1.getCheeses();
-
-      expect(cheeses.length).toBe(2);
-      expect(cheeses[0] instanceof Cheese).toBe(true);
-    });
+  let board1 = await Board.create({
+    type: "checkered",
+    description: "Black & White",
+    rating: 9,
   });
 
-  test("A Cheese can be on many Boards", async () => {
+  let board2 = await Board.create({
+    type: "Soft-cheese",
+    description: "green",
+    rating: 8,
+  });
+
+  await board1.addCheese(cheese1);
+  await board2.addCheese(cheese1);
+
+  expect(board1.title === board2.title).toBe(true);
+});
+
+describe("Eager Loading Checks", () => {
+  test("board can be loaded with its cheeses", async () => {
+    await sequelize.sync({ force: true });
+
+    let board1 = await Board.create({
+      type: "checkered",
+      description: "Black & White",
+      rating: 9,
+    });
+
+    let cheese1 = await Cheese.create({
+      title: "Roquefort",
+      description: "Yellow & green",
+    });
+
+    let cheese2 = await Cheese.create({
+      title: "Quark",
+      description: "Squidgy and slimy",
+    });
+    const boardCheeses = await Board.findAll({ include: Cheese });
+
+    expect(boardCheeses[0] instanceof Cheese);
+  });
+
+  test("user can be loaded with its boards", async () => {
+    await sequelize.sync({ force: true });
+
+    let user1 = await User.create({ name: "Danny", email: "Danny@gmail.com" });
+    expect(user1.name).toBe("Danny");
+    expect(user1.email).toBe("Danny@gmail.com");
+
+    let board1 = await Board.create({
+      type: "checkered",
+      description: "Black & White",
+      rating: 9,
+    });
+
+    let board2 = await Board.create({
+      type: "Soft-cheese",
+      description: "green",
+      rating: 8,
+    });
+
+    const userBoards = await User.findAll({ include: Board });
+    // console.log(userBoards[0]);
+    expect(userBoards instanceof Board);
+    // expect(userBoards.length).toBe(3);
+  });
+
+  test("A cheese can be loaded with its board data", async () => {
     await sequelize.sync({ force: true });
     let cheese1 = await Cheese.create({
       title: "Roquefort",
@@ -115,9 +190,7 @@ describe("model creation tests", () => {
       rating: 8,
     });
 
-    await board1.addCheese(cheese1);
-    await board2.addCheese(cheese1);
-
-    expect(board1.title === board2.title).toBe(true);
+    const cheeseBoard = await Cheese.findAll({ include: Board });
+    expect(cheeseBoard instanceof Board).toBeTruthy;
   });
 });
